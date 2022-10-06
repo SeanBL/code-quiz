18f1b3eb3b7bf24efba636d17d549f8dce032de4
Sean Bristol-Lee <seanbristol81@gmail.com>
2022-10-06 05:13:45 -0700

    added function to start game

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
+var nextCard = document.querySelector(".quiz-card");
 var timerCount;
 var isWin;
 
 function startGame () {
     isWin = false;
     timerCount = 75;
-    startTimer();
     nextQuestion();
 }
 
 function nextQuestion() {
-
+    nextCard.textContent = "Testing";
 }
